7925eab3b8f3ad17d7630f06e2d6499f8428ede2
Denn009 <seleznev.denis009@gmail.com>
2022-11-24 11:32:11 +0300

    fix bg_header

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,6 +1,8 @@
 const burger = document?.querySelector('[data-burger]');
 const nav = document?.querySelector('[data-nav]');
 const navItems = nav?.querySelectorAll('a');
+const header = document?.querySelector('.header');
+const logo = document?.querySelector('[data-logo]')
 const body = document.body;
 
 
@@ -9,6 +11,7 @@ burger?.addEventListener('click', () => {
   body.classList.toggle('stop-scroll');
   burger?.classList.toggle('burger--active');
   nav?.classList.toggle('nav--visible');
+  header?.classList.add('header-active');
 });
 
 navItems.forEach(el => {
@@ -18,3 +21,9 @@ navItems.forEach(el => {
     nav?.classList.remove('nav--visible');
   });
 });
+
+logo.addEventListener('click', () => {
+  body.classList.remove('stop-scroll');
+  burger?.classList.remove('burger--active');
+  nav?.classList.remove('nav--visible');
+})
